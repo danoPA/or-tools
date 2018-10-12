93aca9d3567ec2ae49909890f777ad705fb522fd
Laurent Perron <lperron@google.com>
2018-10-12 10:31:31 +0200

    sync cpp examples with internal code

--- examples/cpp/tsp.cc
+++ examples/cpp/tsp.cc
@@ -11,132 +11,173 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//
+// Traveling Salesman Sample.
+//
+// This is a sample using the routing library to solve a Traveling Salesman
+// Problem.
+// The description of the problem can be found here:
+// http://en.wikipedia.org/wiki/Travelling_salesman_problem.
+// For small problems one can use the hamiltonian path library directly (cf
+// graph/hamiltonian_path.h).
+// The optimization engine uses local search to improve solutions, first
+// solutions being generated using a cheapest addition heuristic.
+// Optionally one can randomly forbid a set of random connections between nodes
+// (forbidden arcs).
 
-#include <vector>
-#include <cmath>
-#include "ortools/base/logging.h"
+#include <memory>
+
+#include "absl/memory/memory.h"
+#include "absl/strings/str_cat.h"
+#include "google/protobuf/text_format.h"
+#include "ortools/base/callback.h"
+#include "ortools/base/commandlineflags.h"
+#include "ortools/base/integral_types.h"
+#include "ortools/base/random.h"
 #include "ortools/constraint_solver/routing.h"
+#include "ortools/constraint_solver/routing_index_manager.h"
+#include "ortools/constraint_solver/routing_parameters.h"
+#include "ortools/constraint_solver/routing_parameters.pb.h"
+
+DEFINE_int32(tsp_size, 10, "Size of Traveling Salesman Problem instance.");
+DEFINE_bool(tsp_use_random_matrix, true, "Use random cost matrix.");
+DEFINE_int32(tsp_random_forbidden_connections, 0,
+             "Number of random forbidden connections.");
+DEFINE_bool(tsp_use_deterministic_random_seed, false,
+            "Use deterministic random seeds.");
+DEFINE_string(routing_search_parameters,
+              "local_search_operators {"
+              "  use_path_lns:BOOL_TRUE"
+              "  use_inactive_lns:BOOL_TRUE"
+              "}",
+              "Text proto RoutingSearchParameters (possibly partial) that will "
+              "override the DefaultRoutingSearchParameters()");
 
 namespace operations_research {
-  class DataProblem {
-    private:
-      std::vector<std::vector<int>> locations_;
 
-    public:
-      DataProblem() {
-        locations_ = {
-          {4, 4},
-          {2, 0}, {8, 0},
-          {0, 1}, {1, 1},
-          {5, 2}, {7, 2},
-          {3, 3}, {6, 3},
-          {5, 5}, {8, 5},
-          {1, 6}, {2, 6},
-          {3, 7}, {6, 7},
-          {0, 8}, {7, 8}
-        };
+// Random seed generator.
+int32 GetSeed() {
+  if (FLAGS_tsp_use_deterministic_random_seed) {
+    return ACMRandom::DeterministicSeed();
+  } else {
+    return ACMRandom::HostnamePidTimeSeed();
+  }
+}
 
-        // Compute locations in meters using the block dimension defined as follow
-        // Manhattan average block: 750ft x 264ft -> 228m x 80m
-        // here we use: 114m x 80m city block
-        // src: https://nyti.ms/2GDoRIe "NY Times: Know Your distance"
-        std::array<int, 2> cityBlock = {228/2, 80};
-        for (auto &i: locations_) {
-          i[0] = i[0] * cityBlock[0];
-          i[1] = i[1] * cityBlock[1];
-        }
-      }
+// Cost/distance functions.
 
-      std::size_t GetVehicleNumber() const { return 1;}
-      const std::vector<std::vector<int>>& GetLocations() const { return locations_;}
-      RoutingModel::NodeIndex GetDepot() const { return RoutingModel::kFirstNode;}
-  };
+// Sample function.
+int64 MyDistance(RoutingIndexManager::NodeIndex from,
+                 RoutingIndexManager::NodeIndex to) {
+  // Put your distance code here.
+  return (from + to).value();  // for instance
+}
 
-  /*! @brief Manhattan distance implemented as a callback.
-   * @details It uses an array of positions and
-   * computes the Manhattan distance between the two positions of two different indices.*/
-  class ManhattanDistance: public RoutingModel::NodeEvaluator2 {
-    private:
-      std::vector<std::vector<int64>> distances_;
-    public:
-      ManhattanDistance(const DataProblem& data) {
-      // Precompute distance between location to have distance callback in O(1)
-      distances_ = std::vector<std::vector<int64>>(
-          data.GetLocations().size(),
-          std::vector<int64>(
-            data.GetLocations().size(),
-            0LL));
-      for (std::size_t fromNode = 0; fromNode < data.GetLocations().size(); fromNode++) {
-        for (std::size_t toNode = 0; toNode < data.GetLocations().size(); toNode++) {
-          if (fromNode != toNode)
-            distances_[fromNode][toNode] =
-              std::abs(data.GetLocations()[toNode][0] - data.GetLocations()[fromNode][0]) +
-              std::abs(data.GetLocations()[toNode][1] - data.GetLocations()[fromNode][1]);
+// Random matrix.
+class RandomMatrix {
+ public:
+  explicit RandomMatrix(int size) : size_(size) {}
+  void Initialize() {
+    matrix_ = absl::make_unique<int64[]>(size_ * size_);
+    const int64 kDistanceMax = 100;
+    ACMRandom randomizer(GetSeed());
+    for (RoutingIndexManager::NodeIndex from(0); from < size_; ++from) {
+      for (RoutingIndexManager::NodeIndex to(0); to < size_; ++to) {
+        if (to != from) {
+          matrix_[MatrixIndex(from, to)] = randomizer.Uniform(kDistanceMax);
+        } else {
+          matrix_[MatrixIndex(from, to)] = 0LL;
         }
       }
     }
-
-    bool IsRepeatable() const override {return true;}
-
-    //! @brief Returns the manhattan distance between the two nodes.
-    int64 Run(RoutingModel::NodeIndex FromNode, RoutingModel::NodeIndex ToNode) override {
-      return distances_[FromNode.value()][ToNode.value()];
-    }
-  };
-
-  //! @brief Print the solution
-  //! @param[in] data Data of the problem.
-  //! @param[in] routing Routing solver used.
-  //! @param[in] solution Solution found by the solver.
-  void PrintSolution(
-      const DataProblem& data,
-      const RoutingModel& routing,
-      const Assignment& solution) {
-    LOG(INFO) << "Objective: " << solution.ObjectiveValue();
-    // Inspect solution.
-    int64 index = routing.Start(0);
-    LOG(INFO) << "Route for Vehicle 0:";
-    int64 distance = 0LL;
-    std::stringstream route;
-    while (routing.IsEnd(index) == false) {
-      route << routing.IndexToNode(index).value() << " -> ";
-      int64 previous_index = index;
-      index = solution.Value(routing.NextVar(index));
-      distance += const_cast<RoutingModel&>(routing).GetArcCostForVehicle(previous_index, index, 0LL);
-    }
-    LOG(INFO) << route.str() << routing.IndexToNode(index).value();
-    LOG(INFO) << "Distance of the route: " << distance << "m";
-    LOG(INFO) << "";
-    LOG(INFO) << "Advanced usage:";
-    LOG(INFO) << "Problem solved in " << routing.solver()->wall_time() << "ms";
+  }
+  int64 Distance(RoutingIndexManager::NodeIndex from,
+                 RoutingIndexManager::NodeIndex to) const {
+    return matrix_[MatrixIndex(from, to)];
   }
 
-  void Solve() {
-    // Instantiate the data problem.
-    DataProblem data;
-
-    // Create Routing Model
-    RoutingModel routing(
-        data.GetLocations().size(),
-        data.GetVehicleNumber(),
-        data.GetDepot());
-
-    // Define weight of each edge
-    ManhattanDistance distance(data);
-    routing.SetArcCostEvaluatorOfAllVehicles(NewPermanentCallback(&distance, &ManhattanDistance::Run));
-
-    // Setting first solution heuristic (cheapest addition).
-    RoutingSearchParameters searchParameters = RoutingModel::DefaultSearchParameters();
-    searchParameters.set_first_solution_strategy(FirstSolutionStrategy::PATH_CHEAPEST_ARC);
-
-    const Assignment* solution = routing.SolveWithParameters(searchParameters);
-    PrintSolution(data, routing, *solution);
+ private:
+  int64 MatrixIndex(RoutingIndexManager::NodeIndex from,
+                    RoutingIndexManager::NodeIndex to) const {
+    return (from * size_ + to).value();
   }
+  std::unique_ptr<int64[]> matrix_;
+  const int size_;
+};
+
+void Tsp() {
+  if (FLAGS_tsp_size > 0) {
+    // TSP of size FLAGS_tsp_size.
+    // Second argument = 1 to build a single tour (it's a TSP).
+    // Nodes are indexed from 0 to FLAGS_tsp_size - 1, by default the start of
+    // the route is node 0.
+    RoutingIndexManager manager(FLAGS_tsp_size, 1,
+                                RoutingIndexManager::NodeIndex(0));
+    RoutingModel routing(manager);
+    RoutingSearchParameters parameters = DefaultRoutingSearchParameters();
+    CHECK(google::protobuf::TextFormat::MergeFromString(
+        FLAGS_routing_search_parameters, &parameters));
+
+    // Setting the cost function.
+    // Put a permanent callback to the distance accessor here. The callback
+    // has the following signature: ResultCallback2<int64, int64, int64>.
+    // The two arguments are the from and to node inidices.
+    RandomMatrix matrix(FLAGS_tsp_size);
+    if (FLAGS_tsp_use_random_matrix) {
+      matrix.Initialize();
+      const int vehicle_cost = routing.RegisterTransitCallback(
+          [&matrix, &manager](int64 i, int64 j) {
+            return matrix.Distance(manager.IndexToNode(i),
+                                   manager.IndexToNode(j));
+          });
+      routing.SetArcCostEvaluatorOfAllVehicles(vehicle_cost);
+    } else {
+      const int vehicle_cost =
+          routing.RegisterTransitCallback([&manager](int64 i, int64 j) {
+            return MyDistance(manager.IndexToNode(i), manager.IndexToNode(j));
+          });
+      routing.SetArcCostEvaluatorOfAllVehicles(vehicle_cost);
+    }
+    // Forbid node connections (randomly).
+    ACMRandom randomizer(GetSeed());
+    int64 forbidden_connections = 0;
+    while (forbidden_connections < FLAGS_tsp_random_forbidden_connections) {
+      const int64 from = randomizer.Uniform(FLAGS_tsp_size - 1);
+      const int64 to = randomizer.Uniform(FLAGS_tsp_size - 1) + 1;
+      if (routing.NextVar(from)->Contains(to)) {
+        LOG(INFO) << "Forbidding connection " << from << " -> " << to;
+        routing.NextVar(from)->RemoveValue(to);
+        ++forbidden_connections;
+      }
+    }
+    // Solve, returns a solution if any (owned by RoutingModel).
+    const Assignment* solution = routing.SolveWithParameters(parameters);
+    if (solution != nullptr) {
+      // Solution cost.
+      LOG(INFO) << "Cost " << solution->ObjectiveValue();
+      // Inspect solution.
+      // Only one route here; otherwise iterate from 0 to routing.vehicles() - 1
+      const int route_number = 0;
+      std::string route;
+      for (int64 node = routing.Start(route_number); !routing.IsEnd(node);
+           node = solution->Value(routing.NextVar(node))) {
+        absl::StrAppend(&route, manager.IndexToNode(node).value(), " (", node,
+                        ") -> ");
+      }
+      const int64 end = routing.End(route_number);
+      absl::StrAppend(&route, manager.IndexToNode(end).value(), " (", end, ")");
+      LOG(INFO) << route;
+    } else {
+      LOG(INFO) << "No solution found.";
+    }
+  } else {
+    LOG(INFO) << "Specify an instance size greater than 0.";
+  }
+}
 }  // namespace operations_research
 
 int main(int argc, char** argv) {
-  google::InitGoogleLogging(argv[0]);
-  FLAGS_logtostderr = 1;
-  operations_research::Solve();
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  operations_research::Tsp();
   return 0;
 }
